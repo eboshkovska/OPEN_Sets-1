5991445cc27b12a92d327b0529c7394f0fe437ef
Ema Barabanovska <ebarabanovska@endava.net>
2016-05-03 12:37:14 +0200

    Added unit tests

--- src/scripts/logic/logic.ts
+++ src/scripts/logic/logic.ts
@@ -1,5 +1,10 @@
 module OPENSets.Logic {
   export class Initial {
     public variable = 1;
+
+     Test():string{
+       let testStr ="TEST";
+      return testStr;
+     }
   }
 }
--- src/tests/Game.spec.ts
+++ src/tests/Game.spec.ts
@@ -9,5 +9,13 @@ module OPENSets.Tests {
       expect(test).toBe("First Jasmine test");
       expect(logic.variable).toBe(1);
     });
+
+     it("test2", () => {
+
+      var logic = new OPENSets.Logic.Initial();
+      var testMethod=logic.Test();
+
+      expect(testMethod).toBe("TEST");
+    });
   });
 }
